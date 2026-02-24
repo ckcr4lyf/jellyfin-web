5cec12d084dd1412688aff0f71abf40af18913bb
Raghu Saxena <poiasdpoiasd@live.com>
2026-02-25 00:40:32 +0800

    dont show on first start

--- src/controllers/playback/video/index.js
+++ src/controllers/playback/video/index.js
@@ -352,7 +352,7 @@ export default function (view) {
                 _focus(focusElement);
             }
             toggleSubtitleSync();
-            toggleAspectRatioScale();
+            // toggleAspectRatioScale();
         } else if (currentVisibleMenu === 'osd' && !layoutManager.mobile) {
             _focus(focusElement);
         }
